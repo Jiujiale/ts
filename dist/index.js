5ff656d29539e83a7ddb85e3244fa01897346e4d
jiaqiu <jiaqiu.win@outlook.com>
2023-09-09 10:34:18 +0800

    types

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
 console.log('hello world');
 let age = 25;
+if (age < 50) {
+    age += 10;
+}
+console.log('age: ', age);
+let arr = [10, 'Mosh'];
+let mySize = 2;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 console.log('hello world');
 let age: number = 25;
+if (age < 50) {
+    age += 10
+}
+console.log('age: ', age)
+
+// tuple
+let arr:[number, string] = [10, 'Mosh']
+
+// enum
+const enum Size {Small = 1 , Medium, Large}
+let mySize: Size = Size.Medium
+console.log(mySize)
